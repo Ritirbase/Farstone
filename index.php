7e71d3e2325d2c24bd09becd03642eba546a1fb0
ritirbase <ritirbase@gmail.com>
2017-07-09 18:41:29 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cryotech
+ * Date: 7/9/2017
+ * Time: 2:30 PM
+ */
+
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Game servers for the Farstone community">
+    <meta name="keywords" content="Game, Server, Farstone">
+    <meta name="author" content="Amadeus">
+
+    <title>Farstone Servers</title>
+
+    <link rel="stylesheet" href="css/bulma.css">
+</head>
+
+<body style="background-image: url('img/Farstone_Image4.png'); background-size: 100%; background-repeat: no-repeat;">
+<div class="navbar" style="opacity: 0.9;">
+    <div class="nav-left">
+        <div class="navbar-brand is-unselectable">
+            <a class="navbar-item">
+                <img src="img/Farstone_Icon1.png" alt="">
+                <p>&nbspFarstone Servers</p>
+            </a>
+        </div>
+    </div>
+    <div class="nav-center">
+        <div class="navbar-item is-unselectable" style="background-color: lightgray;">
+            <!-- why does the is-active class not work? -->
+            <div class="navbar-content">
+                <p>
+                    <small class="has-text-dark">Home</small>
+                </p>
+            </div>
+        </div>
+        <a class="navbar-item is-unselectable">
+            <div class="navbar-content">
+                <p>
+                    <small class="has-text-dark">Servers</small>
+                </p>
+            </div>
+        </a>
+        <a class="navbar-item is-unselectable">
+            <div class="navbar-content">
+                <p>
+                    <small class="has-text-dark">Contact</small>
+                </p>
+            </div>
+        </a>
+        <a class="navbar-item is-unselectable">
+            <div class="navbar-content">
+                <p>
+                    <small class="has-text-dark">Donate</small>
+                </p>
+            </div>
+        </a>
+    </div>
+    <div class="nav-right">
+
+    </div>
+</div>
+
+<section class="section" style="background-color: transparent;">
+    <div class="columns">
+        <div class="column">
+
+        </div>
+        <div class="column" style="background-color: transparent;">
+            <div class="box" style="height: 20rem; background-color: white; opacity: 0.7;">
+                <strong>About Us</strong>
+                <p>
+                    Under construction
+                </p>
+            </div>
+            <div class="box" style="height: 20rem; background-color: white; opacity: 0.7;">
+                <strong>Our Goals</strong>
+                <p>
+                    Under construction
+                </p>
+                <p>
+                    (One goal is to finish this website!)
+                </p>
+            </div>
+        </div>
+        <div class="column" style="background-color: transparent;">
+            <div class="box is-flex" style="display: flex; flex-direction: column; height: 20rem; background-color: white; opacity: 0.7;"> <!-- why do I need display: flex? -->
+                <strong>Our Staff</strong>
+                <p>
+                    Our staff are dedicated to providing the best experience we can to anyone and everyone
+                    who is looking for a good time. We aim to moderate our servers in an unbiased, effective
+                    manner to ensure everyone enjoys our servers within the realm of our rules.
+                </p>
+                <p style="flex-grow: 1;"></p>
+                <p style="font-style: italic;">- Amadeus, Fallen Morals, Space Walrus</p>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+
+</html>
